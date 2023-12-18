91751ca2e769df135b1d89f4ce903c99d10bd47c
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-18 13:50:04 +0100

    Established a few functions related to bubleSorting, checking if a value is in range and to ask an input from an user

--- FunctionUtilities.cs
+++ FunctionUtilities.cs
@@ -2,6 +2,75 @@
 {
     public class FunctionUtilities
     {
+        public static bool InRange(int compareValue, int firstValue, int secondValue, bool minInRange = true, bool maxInRange = true)
+        {
+            ReorderVarAsc(ref firstValue, ref secondValue);
+            if (minInRange)
+            {
+                firstValue--;
+            }
+            if (maxInRange)
+            {
+                secondValue++;
+            }
 
+            return compareValue > firstValue && compareValue < secondValue;
+        }
+        public static void ReorderVarAsc(ref int firstValue, ref int secondValue)
+        {
+            if (firstValue > secondValue)
+            {
+                int aux = firstValue;
+                firstValue = secondValue;
+                secondValue = aux;
+            }
+        }
+        public static void ReorderVarAsc(ref int[]values)
+        {
+            for(int i = 0; i < values.Length-1; i++) 
+            {
+                for(int j = i; j<values.Length ; j++)
+                {
+                    if (values[j] > values[i])
+                    {
+                        int aux = values[j];
+                        values[j] = values[i];
+                        values[i] = aux;
+                    }
+                }
+            }
+        }
+        public static void ReorderVarDesc(ref int firstValue, ref int secondValue)
+        {
+            if (firstValue < secondValue)
+            {
+                int aux = firstValue;
+                firstValue = secondValue;
+                secondValue = aux;
+            }
+        }
+        public static void ReorderVarDesc(ref int[] values)
+        {
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                for (int j = i; j < values.Length; j++)
+                {
+                    if (values[j] < values[i])
+                    {
+                        int aux = values[j];
+                        values[j] = values[i];
+                        values[i] = aux;
+                    }
+                }
+            }
+        }
+
+        public static int AskUserValue(string askMsg = "Proporcioname un valor: ")
+        {
+            int result;
+            Console.Write(askMsg);
+            result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        }
     }
 }
