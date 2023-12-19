cfa8b4fbf29d0ba4106827a000aa7374aa5d2b93
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-19 11:03:52 +0100

    Made the conversion from string to long array

--- ArrayConverters.cs
+++ ArrayConverters.cs
@@ -4,12 +4,21 @@ namespace Utilities
 {
     public class ArrayConverters
     {
-        public static int[] StrToNumConv(string[] values)
+        public static int[] StrToIntConv(string[] values)
         {
             int[] result = new int[values.Length];
             for(int i = 0; i<values.Length; i++)
             {
                 result[i] = Convert.ToInt32(values[i]);
+            }
+            return result;
+        }
+        public static long[] StrToLongConv(string[] values)
+        {
+            long[] result = new long[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                result[i] = Convert.ToInt64(values[i]);
             }
             return result;
         }
