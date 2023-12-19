1bed938f7cfc72838869d6f87d485532a3bf66b8
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-19 11:00:59 +0100

    Mada a new class to contain the utility functions related to converting an array of an object to another object, usually into another different array of an object

---
+++ ArrayConverters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Utilities
+{
+    public class ArrayConverters
+    {
+        public static int[] StrToNumConv(string[] values)
+        {
+            int[] result = new int[values.Length];
+            for(int i = 0; i<values.Length; i++)
+            {
+                result[i] = Convert.ToInt32(values[i]);
+            }
+        }
+    }
+}
